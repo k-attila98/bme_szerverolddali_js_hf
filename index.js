6245cca5902b3fdb7101e4a4c797c6794d989016
k-attila98 <k.attila199810@gmail.com>
2020-10-06 23:21:44 +0200

    hf3: routes.js done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+require('./routes/routes')(app);
+
 app.use(express.static('static'));
 app.listen(3000, function () {
     console.log('Running on :3000');
--- routes/routes.js
+++ routes/routes.js
@@ -17,3 +17,89 @@ const getOrdersMW = require('../middleware/order/getOrdersMW');
 const getOrderMW = require('../middleware/order/getOrderMW');
 const saveOrderMW = require('../middleware/order/saveOrderMW');
 const delOrderMW = require('../middleware/order/delOrderMW');
+
+module.exports = function (app) {
+    const objRepo = {};
+
+    app.get('/',
+        getTopCustomersMW(objRepo),
+        renderMW(objRepo, 'index'));
+
+    app.get('/login',
+        renderMW(objRepo, 'login'));
+    app.post('/login',
+        checkLoginMW(objRepo),
+        handleWrongLoginMW(objRepo));
+
+    app.get('/register',
+        renderMW(objRepo, 'register'));
+    app.post('/register',
+        registerNewUserMW(objRepo));
+
+    app.get('/password_forgot',
+        renderMW(objRepo, 'lost_password'));
+    app.post('/password_forgot',
+        forgotPasswordMW(objRepo));
+
+    app.use('/order',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        getCustomerMW(objRepo),
+        saveOrderMW(objRepo),
+        renderMW(objRepo, 'order'));
+
+    app.get('/customer/list',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        getCustomersMW(objRepo),
+        renderMW(objRepo, 'dashboard'));
+
+    app.use('/customer/add',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        saveCustomerMW(objRepo),
+        renderMW(objRepo, 'customer_add'));
+
+    app.get('/customer/del/:userid',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        getCustomerMW(objRepo),
+        delCustomerMW(objRepo));
+
+    app.use('/customer/edit/:userid',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        getCustomerMW(objRepo),
+        saveCustomerMW(objRepo),
+        renderMW(objRepo, 'customer_edit'));
+
+    app.get('/customer/order/:userid',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        getCustomerMW(objRepo),
+        getOrdersMW(objRepo),
+        renderMW(objRepo, 'customer_orders'));
+
+    app.use('/customer/order/:userid/add',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        getCustomerMW(objRepo),
+        getOrderMW(objRepo),
+        saveOrderMW(objRepo),
+        renderMW(objRepo, 'order_add'));
+
+    app.get('/customer/order/:userid/:orderid/del',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        getOrderMW(objRepo),
+        delOrderMW(objRepo));
+
+    app.use('/customer/order/:userid/:orderid/edit',
+        authMW(objRepo),
+        checkPrivilegeMW(objRepo),
+        getCustomerMW(objRepo),
+        getOrderMW(objRepo),
+        saveOrderMW(objRepo),
+        renderMW(objRepo, 'order_edit'));
+
+}
